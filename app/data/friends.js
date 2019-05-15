c4af17e9705ca61c1cab153d6efb6b5012e60ca4
Ragamuffin <ivana.l@cox.net>
2019-05-15 13:20:52 -0700

    final changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Matt",
+        "photo":"https://images.pexels.com/photos/1680317/pexels-photo-1680317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400",
         "scores":[
             5,
             1,
@@ -15,21 +15,68 @@ var friends = [
             1
           ]
       }, {
-        "name":"Jim",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Craig",
+        "photo":"https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         "scores":[
             2,
             2,
             2,
-            4,
-            5,
-            1,
             2,
-            5,
-            4,
-            1
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            
           ]
-      }
+      }, {
+        "name":"Samuel",
+        "photo":"https://t4.ftcdn.net/jpg/00/82/56/43/240_F_82564307_sGzLomVZBVurW3Vz3WB3OfGQehSTOc56.jpg",
+        "scores":[
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+      }, {
+        "name":"Marc",
+        "photo":"https://t3.ftcdn.net/jpg/01/81/58/64/240_F_181586447_7Ym0Wm8uDrPFjrUg68TkZhPGAYbLwRtt.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+      } , {
+        "name":"Chris",
+        "photo":"https://https://t3.ftcdn.net/jpg/01/42/43/18/240_F_142431863_O0yDoCtmbCHs6X6gVAmslamSrs4xrsn6.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+      } 
+    ]; 
       
-]
+
 module.exports = friends;
--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -12,6 +12,46 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
       
       console.log(req.body)
+
+      var bestMatch = {
+        name: "",
+        photo: "",
+        friendDifference: 10000
+      };
+
+      var userData = req.body;
+      var userScores = userData.scores;
+      var totalDifference = 0;
+
+      for (var i = 0; i < friends.length; i++) {
+        var currentFriend = friends[i];
+        totalDifference = 0;
+  
+        console.log(currentFriend.name);
+  
+        
+        for (var j = 0; j < currentFriend.scores.length; j++) {
+          var currentFriendScore = currentFriend.scores[j];
+          var currentUserScore = userScores[j];
+  
+          
+          totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+        }
+  
+        
+        if (totalDifference <= bestMatch.friendDifference) {
+          
+          bestMatch.name = currentFriend.name;
+          bestMatch.photo = currentFriend.photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
+  
+      
+      friends.push(userData);
+  
+      
+      res.json(bestMatch);
     });
   
     
